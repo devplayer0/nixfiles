762f8bd7ec9943712b0f99216d73e79da76bf130
Jack O'Sullivan <jackos1998@gmail.com>
2022-02-14 23:47:21 +0000

    Add .envrc

--- homes/castle.nix
+++ homes/castle.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  # So home-manager will inject the sourcing of ~/.nix-profile/etc/profile.d/nix.sh
+  targets.genericLinux.enable = true;
+
   programs = {
     kakoune.enable = true;
   };
