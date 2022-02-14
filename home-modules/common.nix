8355526d27cee74669f77cc1fd0e06922cdbc6c1
Jack O'Sullivan <jackos1998@gmail.com>
2022-02-14 19:56:31 +0000

    home-modules/common: Manage bash, add starship + direnv

--- home-modules/common.nix
+++ home-modules/common.nix
@@ -5,28 +5,45 @@ in
 mkMerge [
   {
     programs = {
-      home-manager = {
-        # Even when enabled this will only be actually installed in standalone mode
-        enable = true;
+      # Even when enabled this will only be actually installed in standalone mode
+      # Note: `home-manager.path` is for telling home-manager is installed and setting it in NIX_PATH, which we should
+      # never care about.
+      home-manager.enable = true;
+
+      bash = {
+        # This not only installs bash but has home-manager control .bashrc and friends
+        enable = mkDefault true;
+      };
+
+      lsd = {
+        enable = mkDefault true;
+      };
+
+      starship = {
+        enable = mkDefault true;
+        settings = {
+          aws.disabled = true;
+        };
+      };
+
+      direnv = {
+        enable = mkDefault true;
+        nix-direnv.enable = true;
       };
 
       htop = {
         enable = true;
         settings = {};
       };
-
-      lsd = {
-        enable = mkDefault true;
-      };
     };
 
     home = {
-      language.base = mkDefault "en_IE.UTF-8";
-
       packages = with pkgs; [
         tree
         iperf3
       ];
+
+      language.base = mkDefault "en_IE.UTF-8";
 
       # The flake passes a default setting, but we don't care about that
       stateVersion = mkForce "22.05";
