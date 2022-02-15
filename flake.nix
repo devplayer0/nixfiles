378b8f317470a46c7394713a02b9a1711ab05707
Jack O'Sullivan <jackos1998@gmail.com>
2022-02-15 20:58:03 +0000

    Rename util.nix to lib.nix

--- flake.nix
+++ flake.nix
@@ -45,7 +45,7 @@
       # Extend a lib with extras that _must not_ internally reference private nixpkgs. flake-utils doesn't, but many
       # other flakes (e.g. home-manager) probably do internally.
       libOverlay = final: prev: {
-        my = import ./util.nix { lib = final; };
+        my = import ./lib.nix { lib = final; };
         flake = flake-utils.lib;
       };
       pkgsLibOverlay = final: prev: { lib = prev.lib.extend libOverlay; };
