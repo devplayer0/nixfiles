09ddeecf4f80c1f8ac4e6bc479ee7c309f30d630
Jack O'Sullivan <jackos1998@gmail.com>
2022-09-09 14:16:05 +0100

    nixos/tower: Enable TLP

--- nixos/boxes/tower/default.nix
+++ nixos/boxes/tower/default.nix
@@ -74,6 +74,16 @@
             dmeventd.enable = true;
           };
           fstrim.enable = true;
+          tlp = {
+            enable = true;
+            settings = {
+              CPU_BOOST_ON_BAT = 0;
+              CPU_SCALING_GOVERNOR_ON_BATTERY = "powersave";
+              START_CHARGE_THRESH_BAT0 = 90;
+              STOP_CHARGE_THRESH_BAT0 = 97;
+              RUNTIME_PM_ON_BAT = "auto";
+            };
+          };
 
           resolved = {
             enable = true;
