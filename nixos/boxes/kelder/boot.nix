1f0c3c41602a8f021afb796e40c0e871bdec5c83
Jack O'Sullivan <jackos1998@gmail.com>
2023-05-28 13:45:27 +0100

    nixos/kelder: Add custom boot splash

--- nixos/boxes/kelder/boot.nix
+++ nixos/boxes/kelder/boot.nix
@@ -1,6 +1,20 @@
-{ pkgs, ... }: {
+{ lib, pkgs, ... }:
+let
+  plymouth-kelder = pkgs.runCommand "plymouth-kelder" {} ''
+    target="$out"/share/plymouth/themes/kelder
+    mkdir -p "$target"
+
+    substituteAll ${./plymouth/kelder.plymouth} "$target"/kelder.plymouth
+    cp ${./plymouth/kelder.script} "$target"/kelder.script
+    cp ${./plymouth/kelder.png} "$target"/kelder.png
+    cp ${./plymouth/bridge.png} "$target"/bridge.png
+  '';
+in
+{
   boot.plymouth = {
     enable = true;
+    themePackages = [ plymouth-kelder ];
+    theme = "kelder";
   };
 
   systemd.services = {
