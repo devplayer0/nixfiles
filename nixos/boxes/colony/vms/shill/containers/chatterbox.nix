6ffa35c414a11ec8dd50770da4ae0ed2be379e84
Jack O'Sullivan <jackos1998@gmail.com>
2026-06-01 20:19:51 +0100

    nixos/chatterbox: Bump mautrix bridges

--- nixos/boxes/colony/vms/shill/containers/chatterbox.nix
+++ nixos/boxes/colony/vms/shill/containers/chatterbox.nix
@@ -200,15 +200,15 @@ in
               enable = true;
               package = pkgs.mautrix-whatsapp.overrideAttrs (o: rec {
                 # TODO: Remove when upgrading nixpkgs
-                version = "26.03";
-                tag = "v0.2603.0";
+                version = "26.05";
+                tag = "v0.2605.0";
                 src = pkgs.fetchFromGitHub {
                   owner = "mautrix";
                   repo = "whatsapp";
                   inherit tag;
-                  hash = "sha256-iLPcMi/6262r+jq3RIkiUckniwTFsaajy5EwYFRaenU=";
+                  hash = "sha256-WlVfGQoP9e/wl98hUJei8O2JMcOKijoEY8XuU/z69Qk=";
                 };
-                vendorHash = "sha256-+B/WF0tt7cqfvM07jHpPZHzI5frQOKhxYMFl4asSPqg=";
+                vendorHash = "sha256-Hi/dZHJHoTTCnxLXgbkcYzuzis4fl5kxb5wMd9fKTY8=";
               });
               environmentFile = config.age.secrets."chatterbox/mautrix-whatsapp.env".path;
               settings = {
@@ -255,14 +255,16 @@ in
             };
 
             # TODO: Remove when upgrading nixpkgs
-            mautrix-meta.package = pkgs.mautrix-meta.overrideAttrs (o: {
+            mautrix-meta.package = pkgs.mautrix-meta.overrideAttrs (o: rec {
+              version = "26.05.1";
+              tag = "v0.2605.1";
               src = pkgs.fetchFromGitHub {
                 owner = "mautrix";
                 repo = "meta";
-                rev = "v0.2511.0";
-                hash = "sha256-Ke5b1Q1QIB2u5fbDmhvwe/HaZX1oycNSIor/9gdmdWA=";
+                inherit tag;
+                hash = "sha256-zpolDtwGulDTiojJPnkj9O0D5b4rgPYQX6A28rvuvM0=";
               };
-              vendorHash = "sha256-vbXV9xa0Q+Sml21QQZ3YUmPzXgrIZRJx0tQx0O4JcHs=";
+              vendorHash = "sha256-+i45bXBhlXPXX24VMS9IJLLX+i4VPnqy5RAH4j88sTA=";
             });
             mautrix-meta.instances = {
               messenger = {
